53c676bb4ded83f3360d615d47ff52ef1b16ac25
ZhanaJikia <zhana.jikia.1@btu.edu.ge>
2021-09-06 00:26:03 +0400

    fix: convert by balance symbol

--- converter.js
+++ converter.js
@@ -3,8 +3,7 @@ const fetch = require('node-fetch')
 
 const converter = async eth => {
   const response  = await fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USDT')
-  const { USDT } = await response.json();
-  
+  const { USDT } = await response.json()
   return USDT * eth
 }
 
--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-const Web3 = require("web3");
+const Web3 = require("web3")
 
-const { converter } = require('./converter')
+//Immediately invoked function expression - IIFE
+(async () => {
 
-const provider = "https://mainnet.infura.io/v3/b8fa89d40a4f4259954d465558d05c0a"
+  const provider = "https://mainnet.infura.io/v3/b8fa89d40a4f4259954d465558d05c0a"
 
-const Web3Client = new Web3(new Web3.providers.HttpProvider(provider));
+  const Web3Client = new Web3(new Web3.providers.HttpProvider(provider))
 
-const minABI = [{
-  constant: true,
-  inputs: [{ name: "_owner", type: "address" }],
-  name: "balanceOf",
-  outputs: [{ name: "balance", type: "uint256" }],
-  type: "function",
-}];
+  const minABI = [
+    {
+      constant: true,
+      inputs: [],
+      name: "symbol",
+      outputs: [{ name: "symbol", type: "string" }],
+      type: "function"
+    },
+    {
+      constant: true,
+      inputs: [{ name: "_owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ name: "balance", type: "uint256" }],
+      type: "function",
+    }
+  ]
 
-async function getBalance(walletAddress, contractAddress) {
-  const contract = new Web3Client.eth.Contract(minABI, contractAddress);
-  const wei = await contract.methods.balanceOf(walletAddress).call();
-  
-  
-  const eth = wei / 1_000_000_000_000_000_000
+  const getBalance = async ({ walletAddress, contractAddress }) => {
+    const contract = new Web3Client.eth.Contract(minABI, contractAddress)
+    const balance = await contract.methods.balanceOf(walletAddress).call()
+    const symbol = await contract.methods.symbol().call()
+    return `${balance / 1.0e+6} ${symbol}`
+  }
 
-  console.log(`${eth} ETH`)
-  console.log(`${await converter(eth)} USDT`)
-}
+  let result = ''
 
-const walletAddress = "0xc1aae9d18bbe386b102435a8632c8063d31e747c";
-const contractAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+  result = await getBalance({
+    walletAddress: '0xc1aae9d18bbe386b102435a8632c8063d31e747c',
+    contractAddress:'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+  })
+  console.log(result)
+  // 175000 WETH
 
-getBalance(walletAddress, contractAddress);
-module.exports = { getBalance };
+  result = await getBalance({
+    walletAddress: '0x8a446971dbb112f3be15bc38c14d44b94d9e94b9',
+    contractAddress:'0xdac17f958d2ee523a2206206994597c13d831ec7'
+  })
+  console.log(result)
+  // 105216876.440195 USDT
+
+})()
